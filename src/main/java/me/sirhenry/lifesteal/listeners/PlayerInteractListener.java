497a58470f229f23d1662999115173a449567754
BuildTools <unconfigured@null.spigotmc.org>
2021-08-15 13:37:30 -0700

    Fixed bug that would take item in players main hand if heart was in off hand and the player right clicked.

--- src/main/java/me/sirhenry/lifesteal/listeners/PlayerInteractListener.java
+++ src/main/java/me/sirhenry/lifesteal/listeners/PlayerInteractListener.java
@@ -15,14 +15,12 @@ public class PlayerInteractListener implements Listener {
     @EventHandler
     public void PlayerInteract(PlayerInteractEvent e) {
 
-        System.out.println(e.getAction());
-        System.out.println(e.getItem().getItemMeta());
-        System.out.println(e.getItem().getItemMeta().getDisplayName());
-        System.out.println(e.getItem().getItemMeta().getLore());
-        if(e.getItem().getType() == Material.NETHER_STAR && e.getItem().getItemMeta().getDisplayName().equals("§cHeart")) {
+        if(e.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.NETHER_STAR) && e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals("§cHeart")) {
             if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+
                 e.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(e.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() + 2);
                 e.getPlayer().getInventory().getItemInMainHand().setAmount(e.getPlayer().getInventory().getItemInMainHand().getAmount() - 1);
+
             }
 
         }
